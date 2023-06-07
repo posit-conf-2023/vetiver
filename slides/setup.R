c45cb6595b7cd2a0b12b625f176cc70cb14d8484
Julia Silge <julia.silge@gmail.com>
2023-06-07 14:07:56 -0600

    Start on slides

---
+++ slides/setup.R
@@ -0,0 +1,11 @@
+knitr::opts_chunk$set(
+  digits = 3,
+  comment = "#>",
+  dev = 'svglite'
+)
+
+# devtools::install_github("gadenbuie/countdown")
+library(countdown)
+library(ggplot2)
+theme_set(theme_minimal(base_size = 16))
+options(cli.width = 70)
