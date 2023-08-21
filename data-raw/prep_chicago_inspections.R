bdf7db960d282f319b2ee3566e4a905ff79d8e0d
Isabel Zimmerman <54685329+isabelizimm@users.noreply.github.com>
2023-08-21 14:33:02 -0400

    Move to food inspection data (#9)
    * move to inspection data
    
    * fix R plot
    
    * Update RStudio proj
    
    * Prep data and divide into original + monitoring
    
    * Add datasets
    
    * Update data to use only past July 2018 (big change)
    
    * Updates to slide deck 1
    
    * Update to slide deck 2
    
    * Set up deployment better
    
    * updates from TODOs
    
    - split inspection_date to month, year
    - updated requirements.txt to include pyarrow
    - updated ci to use requirements.txt rather than a ci-specific
      requirements file
    - fixed TODOs
    
    * update url
    
    * one more tweak
    
    * Rearrange where date munging goes a little
    
    * Update rf model
    
    ---------
    
    Co-authored-by: Julia Silge <julia.silge@gmail.com>

---
+++ data-raw/prep_chicago_inspections.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(arrow)
+library(pins)
+
+board <- board_connect_url(c(
+  inspection_data = 'https://colorado.posit.co/rsc/inspection_data/'
+))
+
+inspections_raw <- board |> pin_read("inspection_data")
+
+inspections <- 
+  as_tibble(inspections_raw) |> 
+  mutate(across(c("facility_type", "risk", "results"), as.factor),
+         inspection_date = ymd(inspection_date)) |> 
+  select(results, inspection_date, everything()) |> 
+  filter(inspection_date >= "2018-07-01")
+
+summary(inspections$inspection_date)
+
+path1 <- here::here("data", "inspections.parquet")
+path2 <- here::here("data", "inspections_monitoring.parquet")
+
+inspections |> 
+  filter(inspection_date < ymd("2023-01-01")) |>
+  arrange(inspection_date) |> 
+  write_parquet(path1)
+
+inspections |> 
+  filter(inspection_date >= ymd("2023-01-01")) |> 
+  arrange(inspection_date) |> 
+  write_parquet(path2)
+
+library(tidymodels)
+set.seed(123)
+inspect_split <- inspections |> 
+  filter(inspection_date < ymd("2023-01-01")) |>
+  arrange(inspection_date) |> 
+  initial_split(prop = 0.8)
+
+inspect_train <- training(inspect_split)
+inspect_test <- testing(inspect_split)
+
+inspect_fit <-
+  workflow(
+    results ~ facility_type + risk + total_violations + inspection_date, 
+    rand_forest(mode = "classification", trees = 1e3)
+  ) |> 
+  fit(data = inspect_train)
+
+augment(inspect_fit, new_data = slice_sample(inspect_test, n = 10)) |> 
+  select(-aka_name)
+
+library(vetiver)
+library(pins)
+
+v <- vetiver_model(inspect_fit, "chicago-inspections-rstats")
+board <- board_connect()
+board |> vetiver_pin_write(v)
+
+vetiver_deploy_rsconnect(
+  board = board, 
+  name = "julia.silge/chicago-inspections-rstats",
+  predict_args = list(type = "prob", debug = TRUE),
+  account = "julia.silge",
+  appName = "chicago-inspections-rstats-model-api",
+  forceUpdate = TRUE
+)
